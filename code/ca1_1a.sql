1ef942bd18d588443dde5489c651470cc4c290be
ace108 <ace108@yahoo.com>
2019-12-07 14:23:18 +0800

    Create ca1_1a.sql

---
+++ code/ca1_1a.sql
@@ -0,0 +1,17 @@
+select
+    DATE_FORMAT(o.orderdate, "%Y-%m-%d") orderdate,
+    od.productid,
+    p.productname, od.orderquantity, od.orderprice,
+    c.customerid, CONCAT(c.first_name, " ", c.last_name) customername
+from
+    products p,
+    orderdetails od,
+    orders o,
+    customers c
+where 
+    od.productid = p.productid and
+    o.orderid = od.orderid and
+    o.orderdate between '2018-01-26' and '2018-01-31' and
+    c.customerid = o.customerid
+order by 
+    o.orderdate   -- not necessary but just want to see if there is anything. interestingly no order for 2018-01-31
