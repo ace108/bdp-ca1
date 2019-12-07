bdeef879ac445dbc71a0c3ee934d686420d2702a
ace108 <ace108@yahoo.com>
2019-12-07 14:26:53 +0800

    Update ca1_1a.sql

--- code/ca1_1a.sql
+++ code/ca1_1a.sql
@@ -1,5 +1,5 @@
 select
-    DATE_FORMAT(o.orderdate, "%Y-%m-%d") orderdate,
+    date_format(o.orderdate, "%Y-%m-%d") orderdate,
     od.productid,
     p.productname, od.orderquantity, od.orderprice,
     c.customerid, CONCAT(c.first_name, " ", c.last_name) customername
@@ -12,6 +12,6 @@ where
     od.productid = p.productid and
     o.orderid = od.orderid and
     o.orderdate between '2018-01-26' and '2018-01-31' and
-    c.customerid = o.customerid
+    c.customerid = o.customerid 
 order by 
     o.orderdate   -- not necessary but just want to see if there is anything. interestingly no order for 2018-01-31
